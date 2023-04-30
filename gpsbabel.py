869b310c454bcc7513004320053ba33090cb601c
Nyanta Anineco <anineco@nifty.com>
2023-04-30 09:07:45 +0900

    path to gpsbabel.exe for win11

--- gpsbabel.py
+++ gpsbabel.py
@@ -11,10 +11,10 @@ def exe(*args):
         flags = 0
     else:
         assert(os.name == 'nt')
-        cmd = 'C:\Program Files (x86)\GPSBabel\gpsbabel.exe'
+        cmd = 'C:\Program Files\GPSBabel\gpsbabel.exe'
         flags = subprocess.CREATE_NO_WINDOW
     try:
-        subprocess.run([cmd, *args], check=True, creationflags=flags)
+        subprocess.run([cmd, *args], check=True, creationflags=flags, capture_output=True)
     except subprocess.CalledProcessError:
         print('外部プログラム ' + cmd + ' の実行に失敗しました', file=sys.stderr)
         sys.exit(1)
