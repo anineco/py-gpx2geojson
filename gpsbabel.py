3001b08c0efec1146f64e0037d82c8fe38afa5e0
Anineco Nyanta <anineco@nifty.com>
2023-12-23 11:46:27 +0900

    return subprocess output

--- gpsbabel.py
+++ gpsbabel.py
@@ -14,14 +14,14 @@ def exe(*args):
         cmd = 'C:\Program Files\GPSBabel\gpsbabel.exe'
         flags = subprocess.CREATE_NO_WINDOW
     try:
-        subprocess.run([cmd, *args], check=True, creationflags=flags, capture_output=True)
+        return subprocess.run([cmd, *args], check=True, creationflags=flags, capture_output=True)
     except subprocess.CalledProcessError:
         print('外部プログラム ' + cmd + ' の実行に失敗しました', file=sys.stderr)
         sys.exit(1)
 
 def main():
     """for debug only."""
-    exe('-V')
+    print(exe('-V').stdout)
 
 if __name__ == '__main__':
     main()
